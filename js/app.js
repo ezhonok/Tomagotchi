4d0d55a1460eccdb0fc8c66eb958be9352c00b3c
Svetlana Polyakova <juchini@Svetlanas-MacBook-Air.local>
2019-04-01 15:29:33 -0500

    Tamagotchi class created

---
+++ js/app.js
@@ -0,0 +1,37 @@
+console.log('up and running');
+
+////Pawmagotchi\\\
+
+//
+
+
+
+class Tamagotchi {
+	constructor (){
+		this.age = 0
+		this.sleepiness = 1 
+		this.boredom = 1
+		this.hunger = 1 
+	}
+}
+
+
+
+const game = {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
