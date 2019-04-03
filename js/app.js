9638067ca3aff2dcddcdcb9d322d3d1fe3da61f7
Svetlana Polyakova <juchini@Svetlanas-MacBook-Air.local>
2019-04-03 01:32:07 -0500

    created logic for stats

--- js/app.js
+++ js/app.js
@@ -2,7 +2,29 @@ console.log('up and running');
 
 ////Pawmagotchi\\\
 
-//
+//Completed:
+// Instantiate your Tomagotchi
+// Display a character of your choice on the screen to represent your pet
+// Add the ability to name your pet.
+// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+// Add buttons to the screen to feed your pet
+
+// In Progress:
+// Display the following metrics for your pet:
+// Hunger (1-10 scale)
+// Sleepiness (1-10 scale)
+// Boredom (1-10 scale)
+// Age
+
+//Pending:
+// turn off the lights
+// play with your pet
+// feed your pet
+// Increase your pet's age every x minutes
+// Your pet should die if Hunger, Boredom, or Sleepiness hits 10.
+// Morph your pet at certain ages.
+// Animate your pet across the screen while it's alive.
+// Style the page.
 
 
 
@@ -31,6 +53,21 @@ class Tamagotchi {
 		$('#buttonplay').append(`🎮`);
 	}
 
+
+	setTimerPromts() {
+	setInterval(
+
+				// this function gets run.....
+				() => {
+				
+					// all code in here gets run every second
+					game.clock++
+					
+				}, 
+				1000
+				)
+}
+
 }
 
 
@@ -43,6 +80,7 @@ const game = {
 	hunger: null,
 	boredom: null,
 	sleepiness: null,
+	age: 0,
 
 	start: function (name) {
 		
@@ -50,9 +88,52 @@ const game = {
 		const woodchuck = new Tamagotchi(name)
 		console.log(woodchuck);
 		this.tamaEssence = woodchuck
-		$('#formContainer').append(`I am ${woodchuck.name} and I am alive! Je pense donc je suis`);
+		$('#formContainer').append(`💕Hi, I am ${woodchuck.name}! Let's be friends!💕`);
 		$('form').remove();
 		this.startTimer()
+	},
+
+	feedMe() {
+		if (this.clock % 3 === 0) {
+				this.hunger++
+				$('#hunger').text("Hunger: " + this.hunger)
+		}
+	},
+
+	playWithMe() {
+		if (this.clock % 5 === 0) {
+				this.boredom++
+				$('#boredom').text("Boredom: " + this.boredom)
+		}
+	},
+
+	lightsOff() {
+		if (this.clock % 7 === 0) {
+			 this.sleepiness++
+			 $('#sleep').text("Sleepiness: " + this.sleepiness)
+
+		}
+	},
+
+	aging() {
+		if (this.clock % 60 === 0) {
+			this.age++
+			$('#age').text("Age: " + this.age)
+		}
+	},
+
+	feedMePrompt() {
+		if (this.hunger === 5) {
+		}
+
+	},
+
+	playWithMePrompt() {
+
+	},
+
+	lightsOffPrompt() {
+
 	},
 
 
@@ -63,18 +144,16 @@ const game = {
 
 			// this function gets run.....
 			() => {
-				if (game.clock % 3 === 0) {
-				game.hunger++
-			}
-				if (game.clock % 5 === 0) {
-				game.boredom++
-			}
-				if (game.clock % 7 === 0) {
-				game.sleepiness++
-			}
+				this.feedMe()
+				this.playWithMe()
+				this.lightsOff()
+				this.aging()
+			
 				// all code in here gets run every second
-				game.clock++
+				this.clock++
 				// this.clock++	
+
+			//if hunger reaches 5 - prompt to feed - where to store
 				console.log(game.clock +  ' clock');
 				console.log(game.hunger + ' hunger');
 				console.log(game.sleepiness + ' sleepiness');
